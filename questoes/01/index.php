00d16e5e800173e6033f36f24effe7ddeb91ab1e
AndreBastos72 <129459036+AndreBastos72@users.noreply.github.com>
2025-09-26 09:59:39 -0300

    Update index.php

--- questoes/01/index.php
+++ questoes/01/index.php
@@ -1,30 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
-    <link rel="stylesheet" href="./../../styles.css">
+    <link rel="stylesheet" href="../../styles.css">
 </head>
-
 <body>
-
+    
     <header>
         <h2>Questão 1: Antecessor de um Valor</h2>
     </header>
 
     <main>
+        <?php
+            $numero = 10; // Você pode trocar este número por qualquer outro
+            $antecessor = $numero - 1;
 
-    <?php
-    $valor = 100;
-
-    echo "O antecessor do valor " . $valor . " é " . ($valor - 1) ;
-    
-    ?>
-     
+            echo "<p>O número escolhido foi <strong>$numero</strong>.</p>";
+            echo "<p>O seu antecessor é <strong>$antecessor</strong>.</p>";
+        ?>
     </main>
+
 </body>
-
-
 </html>
