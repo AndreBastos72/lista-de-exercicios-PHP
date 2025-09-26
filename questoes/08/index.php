d153ca2b9a3efd6d1067838be3c42b75d3f8c6b4
AndreBastos72 <129459036+AndreBastos72@users.noreply.github.com>
2025-09-26 10:05:24 -0300

    Update index.php

--- questoes/08/index.php
+++ questoes/08/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
     <link rel="stylesheet" href="./../../styles.css">
@@ -15,11 +15,18 @@
     </header>
 
     <main>
+        <?php
+            echo "<h3>Números ímpares de 1 a 20:</h3>";
+            echo "<p>";
 
-    <!-- implementação da solução -->
-     
+            // Loop que começa em 1 e incrementa de 2 em 2
+            for ($i = 1; $i <= 20; $i += 2) {
+                echo $i . " "; // Exibe o número ímpar seguido de um espaço
+            }
+
+            echo "</p>";
+        ?>
     </main>
 </body>
-
 
 </html>
