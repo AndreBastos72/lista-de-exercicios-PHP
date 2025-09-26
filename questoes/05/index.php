2d859e14dedbfa7e9000889cdf44c69cb740f295
AndreBastos72 <129459036+AndreBastos72@users.noreply.github.com>
2025-09-26 10:04:17 -0300

    Update index.php

--- questoes/05/index.php
+++ questoes/05/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
     <link rel="stylesheet" href="./../../styles.css">
@@ -15,11 +15,20 @@
     </header>
 
     <main>
+        <?php
+            // Define os três números para comparação
+            $num1 = 45;
+            $num2 = 82;
+            $num3 = 33;
 
-    <!-- implementação da solução -->
-     
+            // Usa a função max() para encontrar o maior valor entre eles
+            $maiorNumero = max($num1, $num2, $num3);
+
+            // Exibe os números e o resultado
+            echo "<p>Os números fornecidos foram: <strong>$num1</strong>, <strong>$num2</strong> e <strong>$num3</strong>.</p>";
+            echo "<h3>O maior número entre eles é o <strong>$maiorNumero</strong>.</h3>";
+        ?>
     </main>
 </body>
-
 
 </html>
